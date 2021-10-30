13bd7ffd5b8ce7852b728cff1e61e9dd3883e789
Adam <shihadam1@gmail.com>
2021-10-29 22:09:57 -0700

    Finished FizzBuzz

---
+++ FizzBuzz/fizzbuzz_adam.ts
@@ -0,0 +1,20 @@
+// FizzBuzz
+// Adam Shih
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        let s = "";
+        
+        if (i % 3 == 0) {
+            s += "Fizz";
+        }    
+
+        if (i % 5 == 0) {
+            s += "Buzz";
+        }
+
+        console.log(s == "" ? i : s);
+    }
+}
+
+fizzBuzz();
